3a8883f3a1e14ffa36d930cf7ad90f02c9bbd8c7
ueslenart <ueslenpft@gmail.com>
2025-09-02 16:06:55 -0300

    deixando o jogo mais facil 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numerosLimite = 100;
+let numerosLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas= 1;
 
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 exibirTextoNaTela("h1", 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
