07e0f2df0a51980bbad1f0e964f574d18de3c5c9
ueslenart <ueslenpft@gmail.com>
2025-09-02 15:31:50 -0300

    alterando o numero do limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numerosLimite = 10;
+let numerosLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas= 1;
 
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 exibirTextoNaTela("h1", 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
